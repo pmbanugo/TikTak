74bbe0b0dc41eef78d97f71799915eb2c9cbb96f
Peter Mbanugo <chuksgerrard@yahoo.com>
2014-07-08 12:08:24 +0100

    Added Test for the computer player
    Added Test for the computer player

---
+++ src/TikTak.Test/Core/ComputerPlayerTest.cs
@@ -0,0 +1,29 @@
+﻿using TikTak.Core;
+using TikTak.Core.Interfaces;
+using Xunit;
+
+namespace TikTak.Test.Core
+{
+    public class ComputerPlayerTest
+    {
+        private IComputerPlayer _computer;
+        private IGameService _gameService;
+
+        public ComputerPlayerTest()
+        {
+            _gameService = new GameService(new GameManager(new Board()));
+            _computer = new ComputerPlayer(_gameService);
+        }
+
+        [Fact]
+        public void ShouldMakeMove()
+        {
+            var result = false;
+            _gameService.GameBoardUpdated += (sender, args) => result = true;
+
+            _computer.MakeMove();
+
+            Assert.True(result);
+        }
+    }
+}
